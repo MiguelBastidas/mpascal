bc90c7d6862a4492a35dcb474b58de45fd40123e
Miguel Bastidas <mig.bast00@gmail.com>
2018-06-06 21:10:51 -0400

    fix factorial, add combination function.

--- mpascal.s
+++ mpascal.s
@@ -1,40 +1,90 @@
 	.data
-string: .asciiz "factorial de: "
-nline: 	.asciiz "\n"
+string: .asciiz	"input two numbers to compute nCr.\nn: "
+string2:.asciiz "r: "
+nline:	.asciiz "\n"
 
 	.text
-main:
+main:		
 	li	$v0, 4
-	la	$a0,string
+	la	$a0, string
 	syscall
 
-	li	$v0, 5
+	li	$v0, 5	
 	syscall
-	
-	move 	$a0, $v0
-	jal	fact
-	
-	move	%a0, %v0
+	move	$t0, $v0
+
+	li	$v0, 4
+	la	$a0, string2
+	syscall
+
+	li	$v0, 5	
+	syscall
+	move	$t1, $v0
+
+	move	$a0, $t0
+	move	$a1, $t1
+	jal	comb
+
+exit:		
+	move	$a0, $v0
 	li	$v0, 1
 	syscall
-	
 
-exit:
+	li	$v0, 4
+	la	$a0, nline
+	syscall
+
 	li	$v0, 10
 	syscall
 
-fact:
-	li	$t0, 1
-	move 	$t1, $a0
-loop:
-	mult	$t0, $t1
-	mflo	$t0
 
-	addi	$t1, $t1, -1
-	bgtz	$t1, loop
-	
-	move	$v0, $t0
-	
+# int fact(int a);
+fact:
+	li	$t8, 1
+	move	$t9, $a0
+loop:
+	beq	$t9, $zero, quit
+	mult	$t8, $t9
+	mflo	$t8
+	addi	$t9, $t9, -1
+	bgtz	$t9, loop
+
+quit:
+	move	$v0, $t8
 	jr	$ra
 
+# int comb(int a, int b);
+# aCb = a!/b!(a-b)!
+# t0 = a, t1 = b
+# t2 = a - b
+# t3 = a!, t4 = b!
+# t5 = (a - b)!
+# t6 = t4 * t5
+comb:
+	addi	$sp, $sp, -4
+	sw	$ra, 0($sp)
+
+	sub	$t2, $a0, $a1
+	move	$t0, $a0
+	move	$t1, $a1
+
+	jal 	fact
+	move	$t3, $v0
+
+	move	$a0, $t1
+	jal 	fact
+	move	$t4, $v0
+
+	move	$a0, $t2
+	jal	fact
+	move	$t5, $v0
+
+	mult	$t4, $t5
+	mflo	$t6
+
+	div	$t3, $t6
+	mflo	$v0
+
+	lw	$ra, 0($sp)
+	jr	$ra
 
